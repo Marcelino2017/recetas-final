db579fd11081531d6f4224f7b01ae368fc14e075
Marcelino Mejia <mmejia1@tecbaco.com>
2020-11-09 13:19:47 -0500

    regitro de cliente

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-/* Route::get('/home', 'HomeController@index')->name('home'); */
+Route::get('/home', 'HomeController@index')->name('home');
 Route::get('index', 'RecetasController');
 
